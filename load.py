38372f1effd23bbaa03005d7000e9fe578797846
Charles Damasceno <swirfneblin@gmail.com>
2024-02-26 16:44:29 -0300

    chore: read from big file

--- load.py
+++ load.py
@@ -4,7 +4,7 @@ import pymysql
 # Função para conectar ao banco de dados MySQL
 def connect_to_mysql():
     return pymysql.connect(
-        host='mysql_db',
+        host='localhost',
         user='test_user',
         password='test_password',
         database='test_db',
@@ -12,19 +12,25 @@ def connect_to_mysql():
         cursorclass=pymysql.cursors.DictCursor
     )
 
+
 # Função para ler os dados do arquivo CSV e inseri-los no banco de dados
 def insert_data_from_csv(file_path):
     connection = connect_to_mysql()
     cursor = connection.cursor()
 
     try:
-        with open(file_path, newline='', encoding='utf-8') as csvfile:
+        count = 0
+        with open(file_path, newline='', encoding='utf-8', errors='ignore') as csvfile:
             reader = csv.DictReader(csvfile, delimiter=';')
             for row in reader:
                 columns = ', '.join(row.keys())
                 values = ', '.join(['%s'] * len(row))
                 sql = f"INSERT INTO test_table ({columns}) VALUES ({values})"
                 cursor.execute(sql, tuple(row.values()))
+                count += 1
+                if count % 10000 == 0:
+                    connection.commit()
+                    print(f"Registro {count} inserido com sucesso!")
         connection.commit()
         print("Dados inseridos com sucesso!")
     except Exception as e:
@@ -33,7 +39,7 @@ def insert_data_from_csv(file_path):
         connection.close()
 
 # Caminho para o arquivo CSV
-file_path = '/app/DADOS/sample.csv'
+file_path = 'DADOS/novo-pesadao.csv'
 
 # Chamada da função para inserir os dados no banco de dados
 insert_data_from_csv(file_path)
